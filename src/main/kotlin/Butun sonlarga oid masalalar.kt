769bb615a2a507684f20a8deb55a8fb1f07e2801
SardorSafarov <zomin000s@gmail.com>
2022-06-21 07:45:26 -0700

    Butun sonlar tugadi

---
+++ src/main/kotlin/Butun sonlarga oid masalalar.kt
@@ -0,0 +1,109 @@
+fun main() {
+    //Butun sonlarga oid masalalar
+    //1-masala
+//    val l = readln().toDouble()
+//    println("$l sm = ${l/100} m")
+    //2-masala
+//    val m = readln().toDouble()
+//    println("%.0f kg %.1f tona".format(m,m/1000))
+    //3-masala
+//    val byte = readLine()!!.toDouble()
+//    println("%.0f Kb ga teng".format(byte/1024))
+    //4-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toDouble()
+//    val b = sp[1].toDouble()
+//    println("%.0f marta joylashtirish mumkin".format(a/b))
+    //5-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toDouble()
+//    val b = sp[1].toDouble()
+//    println("%.0f marta joylashtirish mumkin  %.0f  ortib qolgan qismi".format(a/b,a%b))
+    //6-masala
+//    var a = readln().toInt()
+//    println("O`nlar xonasidagi raqam ".plus(a/10))
+//    println("Birlar xonasidagi raqam ".plus(a%10))
+    //7-masala
+//    val a = readln()
+//    println("Raqamlar yig`indisi = ".plus(a[0].toString().toInt()*1+1*a[1].toString().toInt()))
+    //8-masala
+//    val a = readln()
+//    println(a.reversed())
+    //9-masala
+//   val a = readln()
+//    println(a[1])
+    //10-masala
+//    val a = readln()
+//    println("Birlar xonasidagi raqam ->".plus(a[2]))
+//    println("O`nlar xonasidagi raqam ->".plus(a[1]))
+    //11- masala
+//    val a = readln()
+//    println(a[0].toIntt()+a[1].toIntt()+a[2].toIntt())
+    //12-masala
+//    val a= readln()
+//    println(a.reversed())
+    //13-masala
+//    var a = readln().toInt()
+//    val a1 = a/100
+//    a %=100
+//    println("$a$a1")
+    //14-masala
+//    var a= readln().toInt()
+//    val a1 = a%10
+//    a/=10
+//    println("$a1$a")
+    //15-masala
+//    var a = readln()
+//    println("${a[1]}${a[0]}${a[2]}")
+    //16-masala
+//    var a = readln()
+//    println("${a[0]}${a[2]}${a[1]}")
+    //17-masala
+//    var a= readln()
+//    println(a[0])
+    //18-masala
+//    val a= readln()
+//    println(a[0])
+    //19-masala
+//    println("Sekundni kiriting!!!")
+//    val n = readLine()!!.toInt()
+//    println("Kun boshidan boshlab %s min vaqt o`tdi".format(n/60))
+    //20-masala
+//    println("Sekundni kiriting!!!")
+//    val n = readln().toDouble()
+//    println("Kun boshidan boshlab %.2f soat o`tdi".format(n/3600))
+    //21-masala
+//    println("Sekundni kiriting!!!")
+//    val n = readln().toDouble()
+//    println("Kun boshidan boshlab %.0f min va %s secund vaqt o`tdi".format(n/60,n%60))
+    //22-masala
+//    println("Sekundni kiriting!!!")
+//    val n = readln().toDouble()
+//    println("Kun boshidan boshlab %.0f soat va %s secund vaqt o`tdi".format(n/3600,n%3600))
+//23-masala
+//    println("Sekundni kiriting!!!")
+//    var n = readln().toDouble()
+//    println("Kun boshidan boshlab %.0f soat va %.0f min %s sekund vaqt o`tdi".format(n/3600,(n%3600)/60,(n%3600)%60 ))
+    //24-masala
+//    var  n = readln().toInt()
+//    println(n%7)
+    //25-masala
+//    val n = readln().toInt()
+//    println((n+3)%7)
+//    //26-masala
+//    val n = readln().toInt()
+//    println(n%7+1)
+    //27-masala
+//    val n = arrayOf("Yakshanba", "Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Shanba")
+//    val s = readln().toInt()
+//    println(n[s % 7 + 1])
+
+
+    //29-masala
+
+
+}
+
+fun Char.toIntt(): Int {
+    return this.toString().toInt()
+}
