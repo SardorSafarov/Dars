38e21d759c5ea3a98a5de40600d1f80982b3d10e
SardorSafarov <zomin000s@gmail.com>
2022-06-22 11:11:22 -0700

    shart operatorlari 15-masala

---
+++ src/main/kotlin/Shart operatori.kt
@@ -0,0 +1,190 @@
+import java.lang.Integer.max
+import java.lang.Integer.min
+
+fun main() {
+//1-masala
+//    var a = readln().toInt()
+//    if(a>0)
+//        a++
+//    println(a)
+    //2-masala
+//    var a = readln().toInt()
+//    if(a>0)
+//        a++
+//    else
+//        a-=2
+//    println(a)
+    //3-masala
+//    var a = readln().toInt()
+//    when {
+//        a > 0 -> println(++a)
+//        a < 0 -> println(-2 + a)
+//        else -> println(10)
+//    }
+    //4-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    val c = sp[2].toInt()
+//    var s = 0
+//    if (a > 0)
+//        s++
+//    if (b > 0)
+//        s++
+//    if (c > 0)
+//        s++
+//    println("%s ta musbat son bor".format(s))
+    //5-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    val c = sp[2].toInt()
+//    var musbatlarSoni=0
+//    var manfiylarSoni=0
+//
+//    if(a>0)
+//        musbatlarSoni++
+//    else
+//        manfiylarSoni++
+//    if(b>0)
+//        musbatlarSoni++
+//    else
+//        manfiylarSoni++
+//    if(c>0)
+//        musbatlarSoni++
+//    else
+//        manfiylarSoni++
+//    println("Kiritilhan sonlardan %s tasi musbat\n%s tasi manfiy sonlar".format(musbatlarSoni,manfiylarSoni))
+    //6-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    if(a>b)
+//        println("Birinchi son katta!!")
+//    else
+//        println("Ikkinchi son katta!!")
+    //7-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    if(a>b)
+//        println(1)
+//    else
+//        println(2)
+    //8-masala
+//    val sp = readln().split(" ")
+//    val a= sp[0].toInt()
+//    val b= sp[1].toInt()
+//    if(a>b) {
+//        println(a)
+//        println(b)
+//    }else{
+//        println(b)
+//        println(a)
+//    }
+    //9-masala
+//    val sp = readln().split(" ")
+//    var a = sp[0].toInt()
+//    var b = sp[1].toInt()
+//    var c: Int
+//    if (a > b) {
+//        c = a
+//        a = b
+//        b = c
+//    }
+//    println("a=%s va b=%s".format(a,b))
+
+    //10-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    if (a.equals(b))
+//        println(0)
+//    else
+//        println(a.plus(b))
+//    println("a = %s  b = %s".format(a, b))
+    //11-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    if(!a.equals(b))
+//        println(max(a,b))
+//    else
+//        println(0)
+//    println("a=%s b=%s".format(a,b))
+    //12-masala
+    //1-usul
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    val c = sp[2].toInt()
+//    if(b>a && c>a)
+//        println(a)
+//    if(a>b && c>b)
+//        println(b)
+//    if(a>c && b>c)
+//        println(c)
+    //2-usul
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    val c = sp[2].toInt()
+//    println(min(a, min(b,c)))
+
+    //13-masala
+//    val sp = readln().split(" ")
+//    val a = sp[0].toInt()
+//    val b = sp[1].toInt()
+//    val c = sp[2].toInt()
+//    if (a < b && b < c)
+//        println(b)
+//    if (a < c && c < b)
+//        println(c)
+//    if (b < a && a < c)
+//        println(a)
+//    if (b < c && c < a)
+//        println(c)
+//    if (c < a && a < b)
+//        println(a)
+//    if (c < b && b < a)
+//        println(b)
+    //14-masala
+    //1-usul
+//    val sp = readln().split(" ")
+//    val a= sp[0].toInt()
+//    val b= sp[1].toInt()
+//    val c= sp[2].toInt()
+//    if(a<b && a<c)
+//    {
+//        println(a)
+//        if(b>c)
+//            println(b)
+//        else
+//            println(c)
+//    }
+//    if(b<a && b<c)
+//    {
+//        println(b)
+//        if(a>c)
+//            println(a)
+//        else
+//            println(c)
+//    }
+//    if(c<a && c<b)
+//    {
+//        println(c)
+//        if(a>b)
+//            println(a)
+//        else
+//            println(b)
+//    }
+    //2-usul
+//    val sp = readln().split(" ")
+//    val a= sp[0].toInt()
+//    val b= sp[1].toInt()
+//    val c= sp[2].toInt()
+//    println("min=%s\nmax=%s".format(min(a, min(b,c)), max(a, max(b,c))))
+  //15-masala
+
+
+}
