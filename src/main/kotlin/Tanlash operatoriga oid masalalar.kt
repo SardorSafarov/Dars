cfc4b279bf0e677a4b89aa89a6b539a966f6e352
SardorSafarov <zomin000s@gmail.com>
2022-06-23 07:31:22 -0700

    tanlash operatori 6-masala

---
+++ src/main/kotlin/Tanlash operatoriga oid masalalar.kt
@@ -0,0 +1,74 @@
+fun main() {
+    //1-masala
+//    val a = readln().toInt()
+//    when(a)
+//    {
+//        1-> println("Dushanba")
+//        2-> println("Seshanba")
+//        3-> println("Chorshanba")
+//        4-> println("Payshanba")
+//        5-> println("Juma")
+//        6-> println("Shanba")
+//        7-> println("Yakshanba")
+//        else-> println("Bunday kun yo`q")
+//    }
+ //2-masala
+//    val a = readln().toInt()
+//    when(a)
+//    {
+//        1-> println("yomon")
+//        2-> println("qoniqarsiz")
+//        3-> println("qoniqarli")
+//        4-> println("yaxshi")
+//        5-> println("a`lo")
+//        else-> println("Bunday baho yo`q")
+//    }
+    //3-masala
+//    val a = readln().toInt()
+//    when(a)
+//    {
+//        in 1..2-> println("qish")
+//        in 3..5-> println("bahor")
+//        in 6..8-> println("yoz")
+//        in 9..11-> println("kuz")
+//         12-> println("qish")
+//        else-> println("Bundayi yo`q")
+//    }
+    //4-masala
+//    val a = readln().toInt()
+//    when(a)
+//    {
+//        1-> println(31)
+//        2-> println(28)
+//        3-> println(31)
+//        4-> println(30)
+//        5-> println(30)
+//        6-> println(30)
+//        7-> println("30")
+//        8-> println("31")
+//        9-> println("30")
+//        10-> println("31")
+//        11-> println("30")
+//        12-> println("31")
+//        else-> println("Bunday oy yo`q")
+//    }
+    //5-masala
+//    val sp  = readln().split(" ")
+//    val a= sp[0].toInt()
+//    val b= sp[1].toInt()
+//    val amal= sp[2]
+//    when(amal)
+//    {
+//        "+"-> println(a+b)
+//        "-"-> println(a-b)
+//        "*"-> println(a*b)
+//        "/"-> println(a/b)
+//        else-> println("Bundayi yo`q")
+//    }
+    //6-masala
+
+
+
+
+
+}
