b33b485904d7532bd4bbdcf7108088cab1d6e77e
SardorSafarov <zomin000s@gmail.com>
2022-06-24 06:41:56 -0700

    tanlash operatori tugadi

--- src/main/kotlin/Tanlash operatoriga oid masalalar.kt
+++ src/main/kotlin/Tanlash operatoriga oid masalalar.kt
@@ -15,7 +15,7 @@ fun main() {
 //        7-> println("Yakshanba")
 //        else-> println("Bunday kun yo`q")
 //    }
- //2-masala
+    //2-masala
 //    val a = readln().toInt()
 //    when(a)
 //    {
@@ -103,19 +103,7 @@ fun main() {
 //    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-        //10-masala
+    //10-masala
 //    val sp = readln().split(" ")
 //    val a = sp[0]
 //    val b = sp[1].toInt()
@@ -134,7 +122,7 @@ fun main() {
 //        0-> print("Harakatni davom ettir")
 //        else-> print("error")
 //    }
- //   11-masala
+    //   11-masala
 //    val sp = readln().split(" ")
 //    val a = sp[0]
 //    val k1 = sp[1].toInt()
@@ -268,7 +256,7 @@ fun main() {
 //        else -> println("Xato!!")
 //    }
 
- //12-masala
+    //12-masala
 //    val sp = readln().split(" ")
 //    val a = sp[0].toInt()
 //    val x = sp[1].toDouble()
@@ -329,5 +317,210 @@ fun main() {
 //        }
 //    }
     //14-masala
+//    val sp = readln().split(" ")
+//    val n = sp[0].toInt()
+//    val x = sp[1].toDouble()
+//    when(n)
+//    {
+//        1->
+//        {
+//          val a = x
+//          val r1 = a* sqrt(3.0)/6
+//          val r2 = r1*2
+//          val s = a*a* sqrt(3.0)/4
+//            println("Tomoni berildi!!\na=%s\nr1=%s\nr2=%s\ns=%s".format(a,r1,r2,s))
+//        }
+//        2->
+//        {
+//            val r1 = x
+//            val a = r1*6/ sqrt(3.0)
+//            val r2 = r1*2
+//            val s = a*a* sqrt(3.0)/4
+//            println("Ichki chizilgan aylana radiusi berildi!!\na=%s\nr1=%s\nr2=%s\ns=%s".format(a,r1,r2,s))
+//        }
+//        3->{
+//            val r2 = x
+//            val r1 = r2/2
+//            val a = r1*6/ sqrt(3.0)
+//            val s = a*a* sqrt(3.0)/4
+//            println("Tashqi chizilgan aylana radiusi berildi!!\na=%s\nr1=%s\nr2=%s\ns=%s".format(a,r1,r2,s))
+//        }
+//        4->{
+//            val s = x
+//            val a = sqrt(s)*4/ sqrt(3.0)
+//            val r1 = a* sqrt(3.0)/6
+//            val r2 = r1*2
+//            println("Tashqi chizilgan aylana radiusi berildi!!\na=%s\nr1=%s\nr2=%s\ns=%s".format(a,r1,r2,s))
+//        }
 
+//    }
+    //15-masala
+//    val sp = readln().split(" ")
+//    val n = sp[0].toInt()
+//    val m = sp[1].toInt()
+//    when (n){
+//        6-> print("olti")
+//        7-> print("yetti")
+//        8-> print("sakiz")
+//        9-> print("to`qqiz")
+//        10-> print("O`n")
+//        11-> print("Valet")
+//        12-> print("Dama")
+//        13-> print("Qirol")
+//        14-> print("Tuz")
+//    }
+//    print(" ")
+//    when(m){
+//        1-> print("G`isht")
+//        2-> print("Olma")
+//        3-> print("Chillik")
+//        4-> print("Qarg`a")
+//    }
+    //16-masala
+//    val sp = readln().split(" ")
+//    val age = sp[0].toInt()
+//    val a = age/10
+//    val b = age%10
+//    when(a){
+//        2-> print("Yigirma")
+//        3-> print("O`ttiz")
+//        4-> print("Qirq")
+//        5-> print("Ellik")
+//        6-> print("Oltmish")
+//    }
+//    print(" ")
+//    when(b)
+//    {
+//        1-> print("Bir")
+//        2-> print("ikki")
+//        3-> print("uch")
+//        4-> print("To`rt")
+//        5-> print("Besh")
+//        6-> print("Olti")
+//        7-> print("Yetti")
+//        8-> print("Sakkiz")
+//        9-> print("To`qqiz")
+//    }
+//
+//    println(" yosh")
+    //17-masala
+
+//    val sp = readln().split(" ")
+//    val age = sp[0].toInt()
+//    val a = age / 10
+//    val b = age % 10
+//    when (a) {
+//        1-> print("O`n")
+//        2 -> print("Yigirma")
+//        3 -> print("O`ttiz")
+//        4 -> print("Qirq")
+//        5 -> print("Ellik")
+//        6 -> print("Oltmish")
+//    }
+//    when (b) {
+//        1 -> print(" Birta")
+//        2 -> print(" ikkita")
+//        3 -> print(" uchta")
+//        4 -> print(" To`rtta")
+//        5 -> print(" Beshta")
+//        6 -> print(" Oltita")
+//        7 -> print(" Yettita")
+//        8 -> print(" Sakkizta")
+//        9 -> print(" To`qqizta")
+//        0-> print("ta")
+//    }
+//    print(" masala")
+    //18-masala
+//    val sp = readln().split(" ")
+//    val n = sp[0].toInt()
+//    val a = n / 100
+//    val b = (n / 10) % 10
+//    val c = n % 10
+//    if (n == 100)
+//        print("Yuz")
+//    else {
+//        when (a) {
+//            1 -> print("Bir")
+//            2 -> print("Ikki")
+//            3 -> print("Uch")
+//            4 -> print("To`rt")
+//            5 -> print("Besh")
+//            6 -> print("Olti")
+//            7 -> print("Yetti")
+//            8 -> print("Sakkiz")
+//            9 -> print("To`qqiz")
+//        }
+//        print(" yuz ")
+//        when (b) {
+//            1 -> print("O`n")
+//            2 -> print("Yigirma")
+//            3 -> print("O`ttiz")
+//            4 -> print("Qirq")
+//            5 -> print("Ellik")
+//            6 -> print("Oltmish")
+//            7 -> print("Yetmish")
+//            8 -> print("Sakkson")
+//            9 -> print("To`qson")
+//        }
+//        when (c) {
+//            1 -> print(" Bir")
+//            2 -> print(" ikki")
+//            3 -> print(" uch")
+//            4 -> print(" To`rt")
+//            5 -> print(" Besh")
+//            6 -> print(" Olti")
+//            7 -> print(" Yetti")
+//            8 -> print(" Sakkiz")
+//            9 -> print(" To`qqiz")
+//        }
+//    }
+    //19-masala
+//
+//    val year = readln()
+//    val end = year.last()
+//    when {
+//        end.equals('1') || end.equals('6') -> print("sariq ")
+//        end.equals('2') || end.equals('7') -> print("oq ")
+//        end.equals('3') || end.equals('8') -> print("qora ")
+//        end.equals('4') || end.equals('9') -> print("yashil ")
+//        end.equals('5') || end.equals('0') -> print("qizil ")
+//    }
+//    when(year.toInt()%12)
+//    {
+//        4-> print("sichqon")
+//        5-> print("sigir")
+//        6-> print("yo`lbars")
+//        7-> print("quyon")
+//        8-> print("ajdar")
+//        9-> print("ilon")
+//        10-> print("ot")
+//        11-> print("qo`y")
+//        0-> print("maymun")
+//        1-> print("tovuq")
+//        2-> print("it")
+//        3-> print("to`ng`iz")
+//    }
+    //20-masala
+//    val sp = readln().split("-")
+//    val date1 = sp[0].split(".")
+//    val date2 = sp[1].split(".")
+//    val day1 = date1[0].toInt()
+//    val month1 = date1[1].toInt()
+//    val day2 = date2[0].toInt()
+//    val month2 = date2[1].toInt()
+//    when {
+//        (month1 == 1 && month2 == 2) && (day1 >= 20 && day2 <= 18) -> print("Qovg`a")
+//        (month1 == 2 && month2 == 3) && (day1 >= 19 && day2 <= 20) -> print("Baliq")
+//        (month1 == 3 && month2 == 4) && (day1 >= 21 && day2 <= 19) -> print("Qo`y")
+//        (month1 == 4 || month2 == 5) && (day1 >= 20 && day2 <= 20) -> print("Buzoq")
+//        (month1 == 5 || month2 == 6) && (day1 >= 21 && day2 <= 21) -> print("Egizaklar")
+//        (month1 == 6 || month2 == 7) && (day1 >= 22 && day2 <= 22) -> print("Qisqichbaqa")
+//        (month1 == 7 || month2 == 8) && (day1 >= 23 && day2 <= 22) -> print("Arslon")
+//        (month1 == 8 || month2 == 9) && (day1 >= 23 && day2 <= 22) -> print("Parizod")
+//        (month1 == 9 || month2 == 10) && (day1 >= 23 && day2 <= 22) -> print("Tarozi")
+//        (month1 == 10 || month2 == 11) && (day1 >= 23 && day2 <= 22) -> print("Chayon")
+//        (month1 == 11 || month2 == 12) && (day1 >= 23 && day2 <= 21) -> print("O`qotar")
+//        (month1 == 12 || month2 == 1) && (day1 >= 22 && day2 <= 19) -> print("Echki")
+//        else -> print("Bunday burj oralig`i yo`q")
+//    }
 }
