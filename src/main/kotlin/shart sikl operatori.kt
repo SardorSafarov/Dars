6d735fa2e8d31afe8556e12e623b2be524f9851c
SardorSafarov <zomin000s@gmail.com>
2022-06-26 21:35:21 -0700

    while 1 - masala

---
+++ src/main/kotlin/shart sikl operatori.kt
@@ -0,0 +1,16 @@
+fun main() {
+    //1-masala
+//    val sp = readln().split(" ")
+//    var a= sp[0].toInt()
+//    var b= sp[1].toInt()
+//    while (b>=a)
+//    {
+//        b-=a
+//    }
+//    println(b)
+
+
+
+
+
+}
