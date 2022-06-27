4c9084f2a8eaebd7294407b3f494d6fc59f2ca87
SardorSafarov <zomin000s@gmail.com>
2022-06-27 01:38:04 -0700

    while 11 - masala

--- src/main/kotlin/shart sikl operatori.kt
+++ src/main/kotlin/shart sikl operatori.kt
@@ -1,3 +1,5 @@
+import kotlin.math.sqrt
+
 fun main() {
     //1-masala
 //    val sp = readln().split(" ")
@@ -8,6 +10,129 @@ fun main() {
 //        b-=a
 //    }
 //    println(b)
+    //2-masala
+//    val sp = readln().split(" ")
+//    var a = sp[0].toInt()
+//    var b = sp[1].toInt()
+//    var s = 0
+//    while (b >= a) {
+//        s++
+//        b -= a
+//    }
+//    println(s)
+    //3-masala
+//    val sp = readln().split(" ")
+//    var a = sp[0].toInt()
+//    var b = sp[1].toInt()
+//    var s = 0
+//    while (b >= a) {
+//        s++
+//        b -= a
+//    }
+//    println("Butun qismi = $s\nQoldiq qismi = $b")
+
+    //4-masala
+
+//    val n = readln().toInt()
+//    if(n.toString().toInt() == 1)
+//        println("Ha")
+//    else
+//    {
+//        var s =3
+//        while (s<n.toString().toInt())
+//        {
+//            s*=2
+//        }
+//        if(s==n.toString().toInt())
+//            println("Ha")
+//        else
+//            println("Yoq")
+//
+//    }
+    //5-masala
+
+//    val n = readln().toInt()
+//    if (n.toString().toInt() == 1)
+//        println("0")
+//    else {
+//        var s = 2
+//        var k = 1
+//        while (s < n.toString().toInt()) {
+//            s *= 2
+//            k++
+//
+//        }
+//        println("$k darajadagi")
+//    }
+    //6-masala
+//
+//    var n = readln().toInt()
+//    var s= n
+//    if(n%2==0)
+//    {
+//        while (n>2)
+//        {
+//            s*=(n-2)
+//            n-=2
+//        }
+//
+//
+//    }
+//    else
+//    {
+//        while (n>1)
+//        {
+//            s*=(n-2)
+//            n-=2
+//        }
+//    }
+//    println(s)
+
+    //7-masala
+//    val n =readln().toInt()
+//    var k=1
+//    while (k*k <=n )
+//    {
+//        k++
+//    }
+//    println(k)
+    //  println(sqrt(readln().toDouble()).toInt()+1)
+    //8-masala
+    //  println(sqrt(readln().toDouble()).toInt())
+    //9-masala
+//    val n  = readln().toInt()
+//    var k=1
+//    var s=3
+//    while (s<=n){
+//        s*=3
+//        k++
+//    }
+//    println(k)
+    //10-masala
+//        val n  = readln().toInt()
+//    var k=0
+//    var s=3
+//    while ((s<=n)){
+//        s*=3
+//        k++
+//    }
+//    println(k)
+    //11-masala
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
