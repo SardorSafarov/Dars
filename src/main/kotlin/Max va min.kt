1b7f1eda9eb70d8f6672212dbbbdf5d1002fa932
SardorSafarov <zomin000s@gmail.com>
2022-07-05 03:24:39 -0700

    max va min 10-masala

--- src/main/kotlin/Max va min.kt
+++ src/main/kotlin/Max va min.kt
@@ -1,6 +1,196 @@
 fun main() {
 
+    //1-masala
+//    var n = readln().toInt()
+//    println("Toplam elementlarni kiriting!!!")
+//    var s = true
+//    var max =0
+//    var min =0
+//    for(i in 1..n-2)
+//    {
+//        var a = readln().toInt()
+//        if(s){
+//           max = a
+//           min = a
+//           s = false
+//        }
+//        if(max<a)
+//            max = a
+//        if(min>a)
+//          min = a
+//    }
+//    println("To`plamdagi eng katta son $max\nTo`plamdagi eng kichik son $min")
+    //2-masala
+//    var n = readln().toInt()
+//    var max = 0
+//    var s = true
+//    for (i in 1..n) {
+//        println("To`g`ri to`rt burchak tomonlarni kiriting!!")
+//        var a = readln().toInt()
+//        var b = readln().toInt()
+//        if(s){
+//            s = false
+//            max = a*b
+//            continue
+//        }
+//        if (max<a*b){
+//            max = a*b
+//        }
+//
+//    }
+//    println("Eng katta yuza $max")
+
+    //3-masala
+
+//        var n = readln().toInt()
+//    var max = 0
+//    var s = true
+//    for (i in 1..n) {
+//        println("To`g`ri to`rt burchak tomonlarni kiriting!!")
+//        var a = readln().toInt()
+//        var b = readln().toInt()
+//        if(s){
+//            s = false
+//            max = 2*(a+b)
+//            continue
+//        }
+//        if (max<2*(a+b)){
+//            max = 2*(a+b)
+//        }
+//
+//    }
+//    println("Eng katta piremetr $max")
+
+//4-masala
+//    var n = readln().toInt()
+//    var s = 1
+//    println("To`plam elamentlarni kiriting")
+//    var min = readln().toInt()
+//    for(i in 2..n)
+//    {
+//        var a = readln().toInt()
+//        if(a<min)
+//            s=i
+//    }
+//    println(s)
+    //5-masala
+
+//    var n = readln().toInt()
+//    var max = 0.0
+//    var s = true
+//    for (i in 1..n) {
+//        println("To`g`ri to`rt burchak tomonlarni kiriting!!")
+//        var a = readln().toDouble()
+//        var b = readln().toDouble()
+//        if (s) {
+//            s = false
+//            max = a / b
+//            continue
+//        }
+//        if (max < a / b) {
+//            max = a / b
+//        }
+//
+//    }
+    //6-masala
+//    var n = readln().toInt()
+//    var min = 0
+//    var max = 0
+//    var maxTR = 1
+//    var minTR = 1
+//    var s = true
+//    for (i in 1..n) {
+//        val a = readln().toInt()
+//        if (s) {
+//            s = false
+//            min = a
+//            max = a
+//        }
+//        if (min > a) {
+//            min = a
+//            minTR =i
+//        }
+//        if (max <= a) {
+//            max = a
+//            maxTR = i
+//        }
+//    }
+//    println("Min tartib raqami $minTR")
+//    println("Max tartib raqami $maxTR")
+    //7-masala
+
+//        var n = readln().toInt()
+//    var min = 0
+//    var max = 0
+//    var maxTR = 1
+//    var minTR = 1
+//    var s = true
+//    for (i in 1..n) {
+//        val a = readln().toInt()
+//        if (s) {
+//            s = false
+//            min = a
+//            max = a
+//        }
+//        if (min >= a) {
+//            min = a
+//            minTR =i
+//        }
+//        if (max < a) {
+//            max = a
+//            maxTR = i
+//        }
+//    }
+//    println("Min tartib raqami $minTR")
+//    println("Max tartib raqami $maxTR")
+//
+    //8-masala
+//    var n = readln().toInt()
+//    var s = true
+//    var tr1 = 1
+//    var tr2 = 1
+//    var min = 0
+//
+//    for (i in 1..n) {
+//        var a = readln().toInt()
+//        if (s) {
+//            min = a
+//        }
+//        if (min > a) {
+//            tr1 = i
+//            min = a
+//        }
+//        if (min >= a) {
+//            tr2 = i
+//            min = a
+//        }
+//
+//    }
+//    println("Tartib raqam $tr1\nTartib Raqam oxirgisi $tr2")
+    //9-masala
+//    var n = readln().toInt()
+//    var s = true
+//    var tr1 = 1
+//    var tr2 = 1
+//    var min = 0
+//
+//    for (i in 1..n) {
+//        var a = readln().toInt()
+//        if (s) {
+//            min = a
+//        }
+//        if (min < a) {
+//            tr1 = i
+//            min = a
+//        }
+//        if (min <= a) {
+//            tr2 = i
+//            min = a
+//        }
+//
+//    }
+//    println("Tartib raqam $tr1\nTartib Raqam oxirgisi $tr2")
+    //10-masala
+
+
 }
-
-
-
